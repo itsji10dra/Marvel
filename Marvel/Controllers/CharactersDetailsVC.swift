69719f0fb744ea84840466d08474e2f5ba0f40a7
Jitendra <jitendra@cellpointmobile.com>
2018-04-17 01:05:34 +0530

    Showing character details on Detail View...

--- Marvel/Controllers/CharactersDetailsVC.swift
+++ Marvel/Controllers/CharactersDetailsVC.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SafariServices
 
 class CharactersDetailsVC: UIViewController {
 
     // MARK: - IBOutlets
 
+    @IBOutlet weak var thumbImageView: UIImageView!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    @IBOutlet weak var bottomView: UIView!
+
     // MARK: - Data
 
     var character: Character?
@@ -22,6 +30,44 @@ class CharactersDetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = character?.name
+        configureUI()
+        
+        loadCharacterDetails()
+    }
+    
+    // MARK: - Private
+
+    private func configureUI() {
+        
+        guard character?.urls?.first(where: { $0.type == .details })?.url == nil else { return }
+        
+        bottomView.removeFromSuperview()
+    }
+    
+    private func loadCharacterDetails() {
+        
+        guard let character = character else { return }
+        
+        title = character.name
+        
+        nameLabel.text = character.name
+        
+        let hasDescription = character.description?.isEmpty != true
+        descriptionLabel.text = hasDescription ? character.description : "There is no description for this character."
+        
+        if let imageURL = character.thumbnail?.thumbURL,
+            let data = try? Data(contentsOf: imageURL) {
+            thumbImageView.image = UIImage(data: data)
+        }
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func showMoreDetailsAction(_ sender: Any) {
+        
+        guard let url = character?.urls?.first(where: { $0.type == .details })?.url else { return }
+
+        let safariVC = SFSafariViewController(url: url)
+        navigationController?.present(safariVC, animated: true, completion: nil)
     }
 }
