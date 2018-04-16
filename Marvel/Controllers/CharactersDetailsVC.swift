adaf00d97e6ad337400af352ef7f97115dbacded
Jitendra <jitendra@cellpointmobile.com>
2018-04-16 17:45:10 +0530

    Added controllers and configured storyboard...

---
+++ Marvel/Controllers/CharactersDetailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersDetailsVC.swift
+//  Marvel
+//
+//  Created by Jitendra on 16/04/18.
+//  Copyright © 2018 Jitendra Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class CharactersDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Marvel/Controllers/CharactersListVC.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersListVC.swift
+//  Marvel
+//
+//  Created by Jitendra on 16/04/18.
+//  Copyright © 2018 Jitendra Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class CharactersListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
