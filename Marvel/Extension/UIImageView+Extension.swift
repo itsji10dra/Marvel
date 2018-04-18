fdd29b5438a43789f216352ceebb929b6d5f8d66
Jitendra <jitendra@cellpointmobile.com>
2018-04-19 00:14:42 +0530

    Added indicator within Image View...

--- Marvel/Extension/UIImageView+Extension.swift
+++ Marvel/Extension/UIImageView+Extension.swift
@@ -10,28 +10,63 @@ import UIKit
 import Foundation
 
 private var kURLKey: Void?
+private var kIndicatorKey: Void?
 
 extension UIImageView {
     
-    public var imageURL: URL? {
+    // MARK: - Private
+    
+    private var imageURL: URL? {
         set { objc_setAssociatedObject(self, &kURLKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
         get { return objc_getAssociatedObject(self, &kURLKey) as? URL }
     }
     
-    func setImage(with url: URL) {
+    private var indicator: UIActivityIndicatorView? {
+        get {
+            guard let indicator = objc_getAssociatedObject(self, &kIndicatorKey) as? UIActivityIndicatorView else { return nil }
+            return indicator
+        }
+        set {
+            indicator?.removeFromSuperview()
+            objc_setAssociatedObject(self, &kIndicatorKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    private func getActivityIndicator() -> UIActivityIndicatorView {
+        
+        if let indicator = indicator {
+            return indicator
+        } else {
+            let indicator = UIActivityIndicatorView()
+            indicator.activityIndicatorViewStyle = .gray
+            indicator.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+            self.addSubview(indicator)
+            self.indicator = indicator
+            return indicator
+        }
+    }
+    
+    // MARK: - Public
+    
+    public func setImage(with url: URL) {
         
         self.imageURL = url
         self.image = nil
+        
+        let spinningIndicator = getActivityIndicator()
+        spinningIndicator.startAnimating()
 
         ImageDownloadCachingManager.shared.downloadAndCacheImage(with: url) { [weak self] (image, url) in
             
             DispatchQueue.main.async {
                 
+                spinningIndicator.stopAnimating()
+
                 guard let strongSelf = self,
                     url == strongSelf.imageURL else {
                     return
                 }
-
+                
                 strongSelf.image = image
             }
         }
